017af2ff5c27dde725ad3cf37a5e21685d8ef815
Will <github@cwdd.info>
2013-05-08 19:20:28 -0400

    a hack to make the shared config work right

--- zipit-config.php
+++ zipit-config.php
@@ -1,3 +1,12 @@
 <?php
 
-require_once('../../../../zipit-config.php');
+
+// this is really a hack of a way to get this working but
+// mb bc of namespaces just including another file isn't going to work
+
+$shared_config = (dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/zipit-config.php');
+$contents = file_get_contents($shared_config);
+$contents = trim($contents,'<?php');
+eval($contents);
+
+
